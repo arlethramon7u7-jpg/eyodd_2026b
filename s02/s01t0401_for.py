a9cf2e6c9d39d4115a06d88146ab28728b7c4f76
arlethramon7u7-jpg <arlethramon.7u7@gmail.com>
2026-09-24 16:59:55 +0000

    S01T04

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -8,30 +8,42 @@ calculará la suma del 1 al 100
 #Importamos biblioteca time
 import time
 
-#Tomando el tiempo inicial
-timestamp_01 = time.time()
+#Funcion que suma los
+#primeros "n" numeros naturales
+def sum_of_n(n):
+    total_sum = 0
+    #Sumando los "n" numeros
+    #Ciclo for
+    for number in range(1,n+1):
+      total_sum = total_sum + number
+    #Retornando el total de la suma
+    return total_sum
 
-#Programa que calcula la suma
-#de los "n" números naturales
-n = 100
-total_sum = 0
+#Variable para guardar
+#El data set
+dataset = [] # [(n,time,sum),(n,time,sum)]
 
+#Generando el contenido del Dataset
+for repetition in range(1,11):
+  # Tomando el tiempo (inicial)
+  timestamp_01 = time.time()
 
-#Ciclo for
-for number in range(1,n+1):
-    total_sum = total_sum=0 + number
-    #1: sum<- 0 + 1
-    #sum = 1
-    #2: sum<- 1 + 2
-    #sum = 3
-    #3: sum<- 1 + 3
-    #...
-    #100: sum<- sum_(-1) + 100
-print(f"La suma de 1 hasta {n} es: {total_sum}")
-#Actualizacion del programa de suma
+  # Sumo los "n" numeros
+  n = repetition*500
+  # Guardo el resultado en result
+  result = sum_of_n(n)
 
-#Tomando el tiempo final 
-timestamp_02 = time.time()
+  # ⏱️Tomando el tiempo final 
+  timestamp_02 = time.time()
 
-#Impresion del tiempo de ejecucion 
-print(f"Tiempo de ejecucion: {(timestamp_02-timestamp_01) * 1e6:.2f} μs")
+  # Calculando el tiempo
+  elapsed_time = round ((timestamp_02-timestamp_01) * 1e6,2)
+
+  # Agregar la tripleta de los 
+  # datos al dataset
+  dataset.append( (n,elapsed_time,result) )
+
+# Imprimir el dataset
+for tup in dataset:
+   print(tup)
+
